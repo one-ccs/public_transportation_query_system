482d008f7e3f0d1c8c80cfe7979033adc3e02afd
one-ccs <one-ccs@foxmail.com>
2024-02-27 13:51:35 +0800

    1、后端 完成公告功能

--- backend/src/main/java/com/example/public_transportation_query_system/controller/NoticeController.java
+++ backend/src/main/java/com/example/public_transportation_query_system/controller/NoticeController.java
@@ -1,14 +1,19 @@
 package com.example.public_transportation_query_system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.public_transportation_query_system.entity.po.Notice;
+import com.example.public_transportation_query_system.entity.vo.BasePageQuery;
 import com.example.public_transportation_query_system.entity.vo.Result;
-import com.example.public_transportation_query_system.entity.vo.request.QueryNoticeVO;
+import com.example.public_transportation_query_system.entity.vo.request.DeleteVO;
+import com.example.public_transportation_query_system.service.impl.NoticeServiceImpl;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,27 +23,30 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/api/notice")
 public class NoticeController {
 
+    @Autowired
+    NoticeServiceImpl noticeServiceImpl;
+
     @Operation(summary = "获取公告列表", description = "获取公告列表接口")
     @GetMapping
-    public Result<Object> apiNoticeGet(QueryNoticeVO queryNoticeVO) {
-        return Result.success();
+    public Result<Object> apiNoticeGet(BasePageQuery query) {
+        return noticeServiceImpl.getPageNotices(query);
     }
 
     @Operation(summary = "添加公告", description = "添加公告接口")
     @PutMapping
-    public Result<Object> apiNoticePut() {
-        return Result.success();
+    public Result<Object> apiNoticePut(@RequestBody Notice notice) {
+        return noticeServiceImpl.addNotice(notice);
     }
 
     @Operation(summary = "修改公告", description = "修改公告接口")
     @PostMapping
-    public Result<Object> apiNoticePost() {
-        return Result.success();
+    public Result<Object> apiNoticePost(@RequestBody Notice notice) {
+        return noticeServiceImpl.modifyNotice(notice);
     }
 
     @Operation(summary = "删除公告", description = "删除公告接口")
     @DeleteMapping
-    public Result<Object> apiNoticeDelete() {
-        return Result.success();
+    public Result<Object> apiNoticeDelete(@RequestBody DeleteVO deleteVO) {
+        return noticeServiceImpl.deleteNotice(deleteVO);
     }
 }
--- backend/src/main/java/com/example/public_transportation_query_system/service/impl/NoticeServiceImpl.java
+++ backend/src/main/java/com/example/public_transportation_query_system/service/impl/NoticeServiceImpl.java
@@ -1,12 +1,96 @@
 package com.example.public_transportation_query_system.service.impl;
 
+import java.util.HashMap;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.public_transportation_query_system.entity.po.Notice;
+import com.example.public_transportation_query_system.entity.vo.BasePageQuery;
+import com.example.public_transportation_query_system.entity.vo.Result;
+import com.example.public_transportation_query_system.entity.vo.request.DeleteVO;
 import com.example.public_transportation_query_system.mapper.NoticeMapper;
 import com.example.public_transportation_query_system.service.INoticeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 
 @Service
 public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, Notice> implements INoticeService {
 
+    public Result<Object> getPageNotices(BasePageQuery query) {
+        // 构造查询条件
+        LambdaQueryWrapper<Notice> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.ge(query.getStartDatetime() != null, Notice::getReleaseDatetime, query.getStartDatetime())
+            .le(query.getEndDatetime() != null, Notice::getReleaseDatetime, query.getEndDatetime())
+            .like(StringUtils.isNotBlank(query.getQuery()), Notice::getContent, query.getQuery());
+
+        // 分页
+        Page<Notice> page = new Page<>(
+            Optional.ofNullable(query.getPageIndex()).orElse(1),
+            Optional.ofNullable(query.getPageSize()).orElse(10)
+        );
+
+        // 获取分页数据
+        this.page(page, queryWrapper);
+
+        // 构造返回结构
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("total", page.getTotal());
+        map.put("list", page.getRecords());
+
+        return Result.success("查询成功", map);
+    }
+
+    public Result<Object> addNotice(Notice notice) {
+        // 表单验证
+        if (StringUtils.isBlank(notice.getContent())) {
+            return Result.failure(400, "公告内容不能为空");
+        }
+
+        // 清除 id、发布日期
+        notice.setId(null);
+        notice.setReleaseDatetime(null);
+
+        // 添加公告
+        if (this.save(notice)) {
+            return Result.success("添加成功");
+        }
+        return Result.failure(400, "添加失败");
+    }
+
+    public Result<Object> modifyNotice(Notice notice) {
+        // 表单验证
+        if (notice.getId() == null) {
+            return Result.failure(400, "公告 id 不能为空");
+        }
+
+        // 清除发布日期
+        notice.setReleaseDatetime(null);
+
+        // 修改公告
+        if (this.updateById(notice)) {
+            return Result.success("修改成功");
+        }
+        return Result.failure(400, "修改失败");
+    }
+
+    public Result<Object> deleteNotice(DeleteVO deleteVO) {
+        if (deleteVO.getIds() != null) {
+            // 批量删除
+            if (this.removeBatchByIds(deleteVO.getIds())) {
+                return Result.success("批量删除成功");
+            }
+            return Result.failure(400, "批量删除失败");
+        }
+        else if (deleteVO.getId() != null) {
+            // 单个删除
+            if (this.removeById(deleteVO.getId())) {
+                return Result.success("删除成功");
+            }
+            return Result.failure(400, "删除失败");
+        }
+        return Result.failure(400, "删除失败，参数 id 和 ids 不能同时为空");
+    }
 }
