4ef1bc1b63087cdcaa22e33c226442331820d688
one-ccs <one-ccs@foxmail.com>
2024-05-19 11:48:19 +0800

    1、后端 删除 station_route 表

--- backend/src/main/java/com/example/public_transportation_query_system/service/IStationRouteService.java
+++
@@ -1,8 +0,0 @@
-package com.example.public_transportation_query_system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.public_transportation_query_system.entity.po.StationRoute;
-
-public interface IStationRouteService extends IService<StationRoute> {
-
-}
